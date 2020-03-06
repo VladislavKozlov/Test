8bdd682f75dad77308a18a274e830703aa40c4bf
VladislavKozlov <k2vakosa@yandex.ru>
2020-03-06 20:35:23 +0400

    Update topbar and favicon, move Add button, update icons and fix Delete popup

--- UI/src/app/confirmation-dialog/confirmation-dialog.service.ts
+++ UI/src/app/confirmation-dialog/confirmation-dialog.service.ts
@@ -14,7 +14,7 @@ export class ConfirmationDialogService {
     btnOkText: string = 'OK',
     btnCancelText: string = 'Cancel',
     dialogSize: 'sm' | 'lg' = 'sm'): Promise<boolean> {
-    const modalRef = this.modalService.open(ConfirmationDialogComponent, { size: dialogSize });
+    const modalRef = this.modalService.open(ConfirmationDialogComponent, { size: dialogSize, centered: true });
     modalRef.componentInstance.title = title;
     modalRef.componentInstance.message = message;
     modalRef.componentInstance.btnOkText = btnOkText;
